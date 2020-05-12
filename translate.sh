2f8c31a323f7cd3b810858ab58939d71dc640796
Alexey Sakhnov <alexsakhnov@gmail.com>
2020-05-12 11:59:28 +0300

    Добавлен перевод

---
+++ translate.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+LIB_DIR=hyscancore
+TEXTDOMAIN=hyscancore
+SRC_DIR=(hyscancore)
+
+LANG=${2:-"${LANG}"}
+LANG_DIR=$(echo $LANG | cut -d_ -f1)
+
+POT_FILE=${LIB_DIR}/po/${TEXTDOMAIN}.pot
+PO_FILE=${LIB_DIR}/po/${LANG_DIR}/${TEXTDOMAIN}.po
+
+MO_DIR=${3:-"misc/locale"}
+MO_FILE=${MO_DIR}/${LANG_DIR}/LC_MESSAGES/${TEXTDOMAIN}.mo
+
+case "$1" in
+  pot)
+    echo "Extracting lines from glade files..."
+    find . -name "*.ui" | xargs -L 1 intltool-extract --local --type=gettext/glade
+    echo "Extracting lines from hyscan data schema..."
+    mkdir tmp
+    find . -name layer-schema.xml -exec \
+    sed -n -e 's/^.*name="\([^"]\+\).*/char *s = N_("\1");/p' {}  \; > tmp/extract-data-schema.c
+    echo "Extracting lines from C source files..."
+    find ${SRC_DIR[@]} ./tmp -iname *.c -o -iname *.ui | xargs \
+    xgettext --sort-output \
+    --keyword=_ \
+    --keyword=N_ \
+    --keyword=C_:1c,2 \
+    --keyword=Q_:1g \
+    --keyword=g_dngettext:2,3 \
+    --from-code=UTF-8 \
+    -o ${POT_FILE}
+    rm -r tmp
+    ;;
+
+  mo)
+    echo "Writing ${MO_FILE}..."
+    mkdir -p $(dirname ${MO_FILE})
+    msgfmt --output-file=${MO_FILE} ${PO_FILE}
+    ;;
+
+  po)
+    echo "Writing ${PO_FILE}..."
+
+    mkdir -p $(dirname ${PO_FILE})
+    [ -f ${PO_FILE} ] ||  msginit --no-translator --input=${POT_FILE} --output-file=${PO_FILE}
+
+    msgmerge --update ${PO_FILE} ${POT_FILE}
+    ;;
+
+  *)
+    echo "Usage: $0 {pot|po} [LANG]"
+    echo "       $0 mo [LANG [MO_DIR]]"
+    echo ""
+    echo "pot - Scan project dir and create POT file"
+    echo "po  - Update PO file for the given LANG"
+    echo "mo  - Compile PO file to binary format and place it in MO_DIR"
+    echo ""
+    echo "Example:"
+    echo "    $ $0 pot"
+    echo "    $ $0 po"
+    echo "    $ edit ${PO_FILE}"
+    echo "    $ $0 mo"
+    exit 1
+esac
